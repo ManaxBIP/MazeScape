a9d802b7a7dbaa0db6e6174b436d4d79e398c821
Dalyll Reguia <dalyll.reguia@ynov.com>
2023-04-28 14:02:55 +0200

    base menu added

--- src/MaFenetre.java
+++ src/MaFenetre.java
@@ -9,22 +9,26 @@ import javax.imageio.ImageIO;
 
 public class MaFenetre extends JFrame {
 
-    private JButton helloButton;
-    private JButton goodbyeButton;
+    private JButton playButton;
+    private JButton exitButton;
 
 
     public static void main(String[] args) throws IOException {
 
         MaFenetre fenetre = new MaFenetre();
         fenetre.setVisible(true);
+        fenetre.setResizable(false);
 
     }
 
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == helloButton) {
-            JOptionPane.showMessageDialog(null, "Bonjour !");
-        } else if (e.getSource() == goodbyeButton) {
-            JOptionPane.showMessageDialog(null, "Au revoir !");
+        if (e.getSource() == playButton) {
+            JLayeredPane layerpane = getLayeredPane();
+            layerpane.removeAll();
+            layerpane.revalidate();
+            layerpane.repaint();
+        } else if (e.getSource() == exitButton) {
+            this.dispose();
         }
     }
     public MaFenetre() throws IOException {
@@ -36,15 +40,30 @@ public class MaFenetre extends JFrame {
 
         JFrame f = new JFrame("Ajouter une image dans JPanel");
         JPanel panel = new JPanel();
-        panel.setBounds(50, 50, 250, 250);
+        panel.setBounds(0, 0, 1000, 1000);
         BufferedImage img = ImageIO.read(new File("home.png"));
+        panel.setPreferredSize(new Dimension(img.getWidth(), img.getHeight()));
         JLabel pic = new JLabel(new ImageIcon(img));
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setPreferredSize(new Dimension(img.getWidth(), img.getHeight()));
         panel.add(pic);
 
-        add(panel);
-        helloButton = new JButton("Hello");
-        helloButton.addActionListener( this::actionPerformed);
+        // Création des boutons et ajout au JLayeredPane
+        playButton = new JButton("Play");
+        playButton.setBounds(450, 600, 100, 50);
+        playButton.addActionListener(this::actionPerformed);
+        layeredPane.add(playButton);
+        layeredPane.setLayer(playButton, JLayeredPane.DEFAULT_LAYER);
 
+        exitButton = new JButton("Goodbye");
+        exitButton.setBounds(450, 700, 100, 50);
+        exitButton.addActionListener(this::actionPerformed);
+        layeredPane.add(exitButton); // Ajout du bouton "Goodbye" au-dessus de l'image et du bouton "Hello"
+        layeredPane.setLayer(exitButton, JLayeredPane.DEFAULT_LAYER);
+        layeredPane.add(panel);
+
+
+        add(layeredPane);
         //add(helloButton);
 
     }
